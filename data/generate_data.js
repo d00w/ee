2fa00842d494a6e573a45dfb34ee01d103677203
d00w <wangdegeng@gmail.com>
2024-06-10 19:58:48 +1000

    add trial

--- data/generate_data.js
+++ data/generate_data.js
@@ -25,48 +25,42 @@ var cWorld = {
     height: WORLD_HEIGHT/3
 }
 
+var test_data = []
 // UNIFORM DISTRIBUTION
 // 100 to 1,000
-var u1 = []
-for (var i = 0; i < 10; i++) {
-    u1.push({boxes: Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, (i+1) * 100), world: world});
+for (var i = 1; i < 21; i++) {
+    var aabbs = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, i * 50)
+    test_data.push({type:"uniform", boxes: aabbs, world: world});
 }
+
 // 1,000 to 10,000
-var u2 = []
-for (var i = 0; i < 10; i++) {
-    u2.push({boxes: Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, (i+1) * 1000), world: world});
+for (var i = 3; i < 21; i++) {
+    var aabbs = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, i * 500)
+    test_data.push({type:"uniform", boxes: aabbs, world: world});
 }
 // 10,000 to 100,000
-var u3 = []
-for (var i = 0; i < 10; i++) {
-    u3.push({boxes: Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, (i+1) * 10000), world: world});
+for (var i = 3; i < 21; i++) {
+    var aabbs = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, i * 5000)
+    test_data.push({type:"uniform", boxes: aabbs, world: world});
 }
 
 // CONCENTRATED DISTRIBUTION
-var c1 = []
-for (var i = 0; i < 10; i++) {
-    var p1 = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, (i+1) * 100 / 2)
-    var p2 = Utils.generateAABBs(cWorld, SIZE_MIN, SIZE_MAX, (i+1) * 100 / 2)
-    c1.push({boxes: p1.concat(p2), world: world});
+for (var i = 1; i < 21; i++) {
+    var p1 = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, i * 50 / 2)
+    var p2 = Utils.generateAABBs(cWorld, SIZE_MIN, SIZE_MAX, i * 50 / 2)
+    test_data.push({type:"concerntrated", boxes: p1.concat(p2), world: world});
 }
 
-var c2 = []
-for (var i = 0; i < 10; i++) {
-    var p1 = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, (i+1) * 1000 / 2)
-    var p2 = Utils.generateAABBs(cWorld, SIZE_MIN, SIZE_MAX, (i+1) * 1000 / 2)
-    c2.push({boxes: p1.concat(p2), world: world});
+for (var i = 3; i < 21; i++) {
+    var p1 = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, i * 500 / 2)
+    var p2 = Utils.generateAABBs(cWorld, SIZE_MIN, SIZE_MAX, i * 500 / 2)
+    test_data.push({type:"concerntrated", boxes: p1.concat(p2), world: world});
 }
 
-var c3 = []
-for (var i = 0; i < 10; i++) {
-    var p1 = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, (i+1) * 10000 / 2)
-    var p2 = Utils.generateAABBs(cWorld, SIZE_MIN, SIZE_MAX, (i+1) * 10000 / 2)
-    c3.push({boxes: p1.concat(p2), world: world});
-}
-
-var test_data = {
-    uniform: [u1, u2, u3],
-    concentrated: [c1, c2, c3]
+for (var i = 3; i < 20; i++) {
+    var p1 = Utils.generateAABBs(world, SIZE_MIN, SIZE_MAX, i * 5000 / 2)
+    var p2 = Utils.generateAABBs(cWorld, SIZE_MIN, SIZE_MAX, i * 5000 / 2)
+    test_data.push({type:"concerntrated", boxes: p1.concat(p2), world: world});
 }
 
 fs.writeFile("./test_data.js", "var data="+JSON.stringify(test_data), function(err) {
--- run.js
+++ run.js
@@ -5,65 +5,90 @@ var fs = require('fs')
 var test_data = fs.readFileSync("./data/test_data.js", 'utf-8')
 eval(test_data)
 
+var result_csv = fs.openSync('result.csv', 'w')
+fs.writeSync(result_csv,'Trial,Algorithm,Data type,Number of boxes,Construction time (ms),Query time (ms)\n')
 
-/*******************************************
- * Quad Tree testing
- * ****************************************/
+function runTrial(t) {
 
-// Uniform 
-console.log("UNIFORM")
-for (var i = 0; i < 3; i++) {
-  console.log("SET NUMBER: "+(i+1))
-  for (var j = 0; j < 10; j++) {
-    console.log("TEST NUMBER: "+(j+1))
-    var boxes = (data.uniform[i][j]).boxes
-    var world = (data.uniform[i][j]).world
-  
+  /*******************************************
+   * Quad Tree testing
+   * ****************************************/
+
+  for (i in data) {
+    var boxes = data[i].boxes
+    var world = data[i].world
+    var type = data[i].type
+    var count = boxes.length
     // Construction start time
     var time = process.hrtime();
-  
+
     // CONSTRUCTION OF QUAD TREE
     var quadTree = constructQuadTree(world, boxes);
-  
+
     // Construction end time
     time = process.hrtime(time);
-  
+
     // Print result
+    fs.writeSync(result_csv,t +","+"QUAD Tree," + type + ","+count+","+Utils.toMs(time))//the line is not finished yet
     console.log('QUAD TREE CONSTRUCTION: '+Utils.toMs(time) +'ms');
-  
+
     // Query start time
     time = process.hrtime();
-  
+
     // QUERY OF QUAD TREE
     for (var k = 0; k < quadTree.objects.length; k++) {
         queryQuadTree(quadTree.tree, quadTree.objects[k])
     }
-  
+
     // Query end time
     time = process.hrtime(time);
-  
+
     // Print result
+    fs.writeSync(result_csv,','+Utils.toMs(time)+'\n')
     console.log('QUAD TREE QUERY: '+Utils.toMs(time) +'ms');
+  }
+
+  /*******************************************
+   * AABB Tree testing
+   * ****************************************/
+
+
+  for (i in data) {
+
+    var boxes = data[i].boxes
+    var world = data[i].world
+    var type = data[i].type
+    var count = boxes.length
+    // Construction start time
+    var time = process.hrtime();
+
+    // CONSTRUCTION OF AABB TREE
+    var aabbTree = constructAabbTree(boxes);
+
+    // Construction end time
+    time = process.hrtime(time);
+
+    // Print result
+    fs.writeSync(result_csv,t +","+"AABB Tree," + type + ","+count+","+Utils.toMs(time))
+    console.log('AABB TREE CONSTRUCTION: '+Utils.toMs(time) +'ms');
+
+    // Query start time
+    time = process.hrtime();
+
+    // QUERY OF AABB TREE
+    for(k in aabbTree.objects) {
+      aabbTree.tree.query(aabbTree.objects[k], function(nodeId) {})
+    }
+
+    // Query end time
+    time = process.hrtime(time);
+
+    // Print result
+    fs.writeSync(result_csv,','+Utils.toMs(time)+'\n')
+    console.log('AABB TREE QUERY: '+Utils.toMs(time) +'ms');
   }
 }
 
-
-
-
-/*******************************************
- * AABB Tree testing
- * ****************************************/
-// var time = process.hrtime();
-
-// var planck = createPlanckAabbTree(boxes)
-
-// time = process.hrtime(time);
-
-// console.log('AABB 1: '+Utils.toMs(time) +'ms');
-// time = process.hrtime();
-
-// for(var i=0;i<planck.objects.length;i++) {
-//   planck.tree.query(planck.objects[i], function(nodeId) {})
-// }
-// time = process.hrtime(time);
-// console.log('AABB 1: '+Utils.toMs(time) +'ms');
+for (var t = 1; t<=3;t++) {
+  runTrial(t);
+}
